e30ec5bf8c917007c621175dac7a1b4eeb18a033
黃暐哲 <huangweizhe@huangweizhedeMacBook-Air.local>
2026-08-06 16:37:05 +0800

    initial project setup with data models

---
+++ screening/models.py
@@ -0,0 +1,43 @@
+from django.conf import settings
+from django.contrib.auth.models import AbstractUser
+from django.db import models
+
+# Create your models here.
+
+class User(AbstractUser):
+    birth_date = models.DateField(null=True, blank=True)
+
+class Questionnaire(models.Model):
+    questionnaire_name = models.CharField(max_length=64)
+
+
+    def __str__(self):
+        return self.questionnaire_name
+    
+class Question(models.Model):
+    questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE, related_name='questions')
+    question_text = models.TextField()
+    question_number = models.IntegerField()
+    category = models.CharField(max_length=64)
+
+
+    def __str__(self):
+        return f"{self.question_number}: {self.question_text[:30]}"
+    
+class Submission(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='submissions')
+    questionnaire = models.ForeignKey(Questionnaire, on_delete=models.CASCADE, related_name='submissions')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+    def __str__(self):
+        return f"{self.user} submit at {self.created_at}"
+    
+class Answer(models.Model):
+    submission = models.ForeignKey(Submission, on_delete=models.CASCADE, related_name='answers')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answers')
+    score = models.IntegerField()
+
+
+    def __str__(self):
+        return f"{self.question}: {self.score}"
