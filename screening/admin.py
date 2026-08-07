835d13ddd439679fabc79680abc76e82d97d7b26
minuet878-prog <minuet878@gmail.com>
2026-08-07 16:33:48 +0800

    add custom user admin and question management in admin

--- screening/admin.py
+++ screening/admin.py
@@ -1,2 +1,24 @@
+from django.contrib import admin
+from django.contrib.auth.admin import UserAdmin
 
 # Register your models here.
+from .models import Answer, Question, Questionnaire, Submission, User
+
+
+@admin.register(User)
+class CustomUserAdmin(UserAdmin):
+    fieldsets = UserAdmin.fieldsets + (("birth_date", {"fields": ("birth_date",)}),)
+    add_fieldsets = UserAdmin.add_fieldsets + (("birth_date", {"fields": ("birth_date",)}),)
+
+
+admin.site.register(Questionnaire)
+
+
+@admin.register(Question)
+class QuestionAdmin(admin.ModelAdmin):
+    list_display = ("question_number", "category", "questionnaire", "question_text")
+    ordering = ("question_number",)
+
+
+admin.site.register(Answer)
+admin.site.register(Submission)
