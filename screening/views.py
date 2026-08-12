9cac79af8af805bdf90b6187d1c722bf1beae9af
minuet878-prog <minuet878@gmail.com>
2026-08-12 16:00:14 +0800

    Add user authentication system with Bootstrap UI

--- screening/views.py
+++ screening/views.py
@@ -1,2 +1,52 @@
+from django.contrib.auth import authenticate, login, logout
+from django.db import IntegrityError
+from django.http import HttpResponseRedirect
+from django.shortcuts import render
+from django.urls import reverse
 
 # Create your views here.
+from .models import User
+
+
+def index(request):
+    return render(request, "screening/index.html")
+
+
+def login_view(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            return HttpResponseRedirect(reverse("index"))
+        else:
+            return render(request, "screening/login.html", {"message": "使用者名稱或密碼錯誤"})
+    else:
+        return render(request, "screening/login.html")
+
+
+def logout_view(request):
+    logout(request)
+    return HttpResponseRedirect(reverse("index"))
+
+
+def register(request):
+    if request.method == "POST":
+        username = request.POST["username"]
+        email = request.POST["email"]
+        password = request.POST["password"]
+        confirmation = request.POST["confirmation"]
+        if password != confirmation:
+            return render(request, "screening/register.html", {"message": "密碼必須相同"})
+
+        try:
+            user = User.objects.create_user(username, email, password)
+            user.save()
+        except IntegrityError:
+            return render(request, "screening/register.html", {"message": "使用者名稱已被使用"})
+        login(request, user)
+        return HttpResponseRedirect(reverse("index"))
+    else:
+        return render(request, "screening/register.html")
