b683414f1c9bbd91979bfd4a1af55dc90d24e197
minuet878-prog <minuet878@gmail.com>
2026-09-01 10:25:32 +0800

    fix(settings): fail loudly on missing SECRET_KEY, enable prod security settings

--- config/settings.py
+++ config/settings.py
@@ -14,6 +14,7 @@ import os
 from pathlib import Path
 
 import dj_database_url
+from django.core.exceptions import ImproperlyConfigured
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -25,12 +26,20 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get(
-    "SECRET_KEY", "django-insecure-7qd&wuoyggjpnsapj#(q_=8akk$!^h-d!!9$eumz&h4-+p(b72"
-)
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get("DEBUG", "True") == "True"
+
+if not DEBUG:
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    if not SECRET_KEY:
+        raise ImproperlyConfigured("正式環境缺少 SECRET_KEY 環境變數，請在部署平台設定後再啟動")
+else:
+    SECRET_KEY = os.environ.get(
+        "SECRET_KEY", "django-insecure-7qd&wuoyggjpnsapj#(q_=8akk$!^h-d!!9$eumz&h4-+p(b72"
+    )
 
 ALLOWED_HOSTS = os.environ.get("ALLOWED_HOSTS", "127.0.0.1,localhost").split(",")
 
