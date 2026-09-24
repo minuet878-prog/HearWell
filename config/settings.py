265ac4404c52cfa17556d36473e0f99f22243cf6
minuet878-prog <minuet878@gmail.com>
2026-09-24 11:42:12 +0800

    fix(settings): default DEBUG to False

--- config/settings.py
+++ config/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get("DEBUG", "True") == "True"
+DEBUG = os.environ.get("DEBUG") == "True"
 
 if not DEBUG:
     SECURE_SSL_REDIRECT = True
